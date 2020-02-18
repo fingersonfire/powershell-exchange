8bed26d3285ae6a9cd0383e22d7f99f55667dcb5
FingersOnFire <fingersonfirecom+github@gmail.com>
2020-02-18 12:07:35 +0100

    Create Enable-SaveCopyInSentItemsForSharedMailboxes.ps1

---
+++ Enable-SaveCopyInSentItemsForSharedMailboxes.ps1
@@ -0,0 +1,8 @@
+﻿Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn; 
+
+$sharedmailboxes = Get-Mailbox -Filter {recipienttypedetails -eq "SharedMailbox"} 
+
+foreach($mailbox in $sharedmailboxes){
+    Write-Host "Enabling Save Sent Item in Shared Mailbox folder for :" $mailbox
+    Set-Mailbox $mailbox -MessageCopyForSentAsEnabled $True
+}
