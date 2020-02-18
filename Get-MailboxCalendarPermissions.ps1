021e7ec67a906a9116045d668a7198ab9e45e7be
FingersOnFire <fingersonfirecom+github@gmail.com>
2020-02-18 12:08:41 +0100

    Create Get-MailboxCalendarPermissions.ps1

---
+++ Get-MailboxCalendarPermissions.ps1
@@ -0,0 +1,28 @@
+﻿Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn; 
+
+$mailboxes = get-mailbox -Resultsize Unlimited
+
+$permreport = @()
+
+foreach ($mailbox in $mailboxes) {
+    $calendarfolders = Get-MailboxFolderStatistics $mailbox.alias | Where-Object{$_.FolderType -eq "Calendar"}
+    foreach ( $calendar in $calendarfolders ) {
+
+        $identity = $calendar.Identity -Replace ("\\",":\") # Won't work if the calendar is not in the root folder.
+        $permissions = get-mailboxfolderpermission $identity
+
+        foreach ( $perm in $permissions ) {
+
+
+            $permreport += [PSCustomObject]@{
+                Mailbox = $mailbox
+                GrantedUser = $perm.User
+                Identity = $perm.Identity
+                Access = [string]::join(', ', $perm.AccessRights)
+                Valid = $perm.IsValid
+            } 
+        }
+    }
+}
+
+$permreport | Export-Csv "C:\Temp\CalendarReport.csv"
