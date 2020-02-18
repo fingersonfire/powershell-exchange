c00c755025974bfea2ae5cfe05f936c4fa7fe311
FingersOnFire <fingersonfirecom+github@gmail.com>
2020-02-18 12:05:45 +0100

    Create Disable-DistributionGroupsEmailAddressPolicy.ps1

---
+++ Disable-DistributionGroupsEmailAddressPolicy.ps1
@@ -0,0 +1,16 @@
+﻿Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn; 
+
+
+$MySearchBase = "*"
+
+# For our first step – we load up a variable with the groups we want (filtered by type):
+$MyGroupList = get-adgroup -Filter 'GroupCategory -eq "Distribution" -and GroupScope -eq "Universal"' -SearchBase "$MySearchBase" -Properties Mail
+
+# If you want to validate you got the correct groups in the variable, list out the names of your objects in the variable:
+#$MyGroupList | Select Name, Mail
+
+# Now, for every group in the list, we flip the type to Universal:
+foreach($Group in $MyGroupList){
+    Write-Host "Disabling Email Address Policy for group:" $Group.Name
+    Set-DistributionGroup -Identity $Group.Name -EmailAddressPolicyEnabled $false
+}
