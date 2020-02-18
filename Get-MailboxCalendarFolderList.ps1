7ad3c0bd65901bbec754ecdc36edd667932e97e3
FingersOnFire <fingersonfirecom+github@gmail.com>
2020-02-18 12:08:13 +0100

    Create Get-MailboxCalendarFolderList.ps1

---
+++ Get-MailboxCalendarFolderList.ps1
@@ -0,0 +1,6 @@
+﻿$MailboxAlias = Get-Mailbox | Select-Object alias 
+
+foreach ($Mailbox in $MailboxAlias) { 
+    #Get-MailboxFolderStatistics -Identity $Mailbox.alias | select-object Identity, ItemsInFolder, FolderSize, FolderAndSubfolderSize 
+    Get-MailboxFolderStatistics $Mailbox.alias | Where-Object{$_.FolderType -eq "Calendar"} | Select Identity 
+} 
