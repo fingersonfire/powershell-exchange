8a7555ccdb7da647e8c8b86d5b374d020523d468
FingersOnFire <fingersonfirecom+github@gmail.com>
2020-02-18 12:08:32 +0100

    Create Get-MailboxCalendarList.ps1

---
+++ Get-MailboxCalendarList.ps1
@@ -0,0 +1,7 @@
+﻿Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn; 
+
+$MailboxAlias = Get-Mailbox | Select-Object alias 
+
+foreach ($Mailbox in $MailboxAlias) { 
+    Get-MailboxFolderStatistics $Mailbox.alias | Where-Object{$_.FolderType -eq "Calendar"} | Select Identity 
+} 
